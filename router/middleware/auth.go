7e2f8be12092ccbad03397ab1742dca0623321b1
Harrison <huangharrsion@gmail.com>
2021-10-17 17:14:51 +0800

    进阶 2：用 Makefile 管理 API 项目

--- router/middleware/auth.go
+++ router/middleware/auth.go
@@ -10,6 +10,7 @@ import (
 // AuthMiddleware 进行 Token 校验
 func AuthMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		// Parse the json web token.
 		if _, err := token.ParseRequest(c); err != nil {
 			handler.SendResponse(c, errno.ErrTokenInvalid, nil)
 			c.Abort()
