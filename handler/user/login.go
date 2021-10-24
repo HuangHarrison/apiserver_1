0d86f6abc4f2626dee9bf4d0ad375fa1e4961547
Harrison <huangharrsion@gmail.com>
2021-10-24 18:12:43 +0800

    进阶 9：生成 Swagger 在线文档

--- handler/user/login.go
+++ handler/user/login.go
@@ -9,9 +9,12 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-// Login generates the authentication token
-// if the password was matched with the specified account.
-// 采用明文方式校验账号和密码，登录后产生token
+// @Summary Login generates the authentication token 采用明文方式校验账号和密码，登录后产生token
+// @Produce  json
+// @Param username body string true "Username"
+// @Param password body string true "Password"
+// @Success 200 {string} json "{"code":0,"message":"OK","data":{"token":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE1MjgwMTY5MjIsImlkIjowLCJuYmYiOjE1MjgwMTY5MjIsInVzZXJuYW1lIjoiYWRtaW4ifQ.LjxrK9DuAwAzUD8-9v43NzWBN7HXsSLfebw92DKd1JQ"}}"
+// @Router /login [post]
 func Login(c *gin.Context) {
 	// Binding the data with the user struct.
 	var u model.UserModel
